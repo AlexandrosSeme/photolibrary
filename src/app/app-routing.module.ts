3a230fe8c2a3b6e3388f2eb2ff93a4c3ecafdbfd
Alexandros Seme <alexandros.seme@gmail.com>
2025-03-24 12:15:21 +0200

    Implement routing with initial layout and component structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from './pages/overview/overview.component';
+import { PhotosComponent } from './pages/photos/photos.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { PhotoDetailComponent } from './pages/photo-detail/photo-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OverviewComponent,
+    children: [
+      { path: '', component: PhotosComponent },
+      { path: 'favorites', component: FavoritesComponent },
+      { path: 'photos/:id', component: PhotoDetailComponent }
+    ]
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,27 @@ import { AppComponent } from './app.component';
 import { OverviewComponent } from './pages/overview/overview.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './pages/header/header.component';
+import { PhotosComponent } from './pages/photos/photos.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { PhotoDetailComponent } from './pages/photo-detail/photo-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    OverviewComponent
+    OverviewComponent,
+    HeaderComponent,
+    PhotosComponent,
+    FavoritesComponent,
+    PhotoDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
-    MatDividerModule,
     MatButtonModule
   ],
   providers: [],
